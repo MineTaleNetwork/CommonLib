775cb3a356a1b252802a2fd915a0a837c4a9d62e
oHate <ohateign@gmail.com>
2022-01-04 02:04:56 -0600

    Created the CustomColor class

---
+++ src/main/java/cc/minetale/commonlib/util/CustomColor.java
@@ -0,0 +1,28 @@
+package cc.minetale.commonlib.util;
+
+import net.kyori.adventure.text.format.TextColor;
+
+public class CustomColor {
+
+    public static TextColor DARK_RED       = TextColor.color(192, 57, 43);
+    public static TextColor RED            = TextColor.color(231, 76, 60);
+    public static TextColor DARK_ORANGE    = TextColor.color(211, 84, 0);
+    public static TextColor ORANGE         = TextColor.color(230, 126, 34);
+    public static TextColor DARK_YELLOW    = TextColor.color(243, 156, 18);
+    public static TextColor YELLOW         = TextColor.color(241, 196, 15);
+    public static TextColor DARK_GREEN     = TextColor.color(39, 174, 96);
+    public static TextColor GREEN          = TextColor.color(46, 204, 113);
+    public static TextColor DARK_TURQUOISE = TextColor.color(22, 160, 133);
+    public static TextColor TURQUOISE      = TextColor.color(26, 188, 156);
+    public static TextColor DARK_BLUE      = TextColor.color(41, 128, 185);
+    public static TextColor BLUE           = TextColor.color(52, 152, 219);
+    public static TextColor DARK_PURPLE    = TextColor.color(142, 68, 173);
+    public static TextColor PURPLE         = TextColor.color(155, 89, 182);
+    public static TextColor BLACK          = TextColor.color(44, 62, 80);
+    public static TextColor MIDNIGHT       = TextColor.color(52, 73, 94);
+    public static TextColor DARK_GRAY      = TextColor.color(127, 140, 141);
+    public static TextColor GRAY           = TextColor.color(149, 165, 166);
+    public static TextColor LIGHT_GRAY     = TextColor.color(189, 195, 199);
+    public static TextColor WHITE          = TextColor.color(236, 240, 241);
+
+}
